ba3fab35287d9933977c5c8484ff08dd4494d1e5
Kirithika S <kirithikas09@gmail.com>
2025-05-10 21:56:05 +0530

    Add files via upload

---
+++ digitcount.java
@@ -0,0 +1,13 @@
+package javaprogram;
+
+public class digitcount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=100;
+		int count= (int)(Math.log10(n)+1);
+		System.out.println(count);
+
+	}
+
+}
