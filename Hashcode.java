0ae4833728aa562f5388d860ddba6c99dc1bd900
Kirithika S <kirithikas09@gmail.com>
2025-05-12 21:44:10 +0530

    Update Hashcode.java

--- Hashcode.java
+++ Hashcode.java
@@ -7,7 +7,7 @@ public class Hashcode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int marks[]= {3,4,5,6};
-		System.out.println(marks);
+		System.out.println(marks);//hashcode
 		System.out.println(Arrays.toString(marks));
 
 	}
